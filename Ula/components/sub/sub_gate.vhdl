4fc659da8a2e0fa9292c591139cf762cfb46688c
Antonio Mori <antonio.mori@ufrpe.br>
2025-02-23 15:05:00 -0300

    fix no sub_gate

--- Ula/components/sub/sub_gate.vhdl
+++ Ula/components/sub/sub_gate.vhdl
@@ -3,53 +3,33 @@ use IEEE.std_logic_1164.all;
 
 entity sub_gate is
     port (
-        a_sub_input : in std_logic;  -- Operando A
-        b_sub_input : in std_logic;  -- Operando B
-        sub_borrow_in : in std_logic;  -- Borrow In (deve ser 1 para complemento de dois)
-        y_sub_output : out std_logic; -- Resultado
-        sub_borrow_out : out std_logic -- Borrow Out (inverso do Carry Out)
+        a_sub_input    : in std_logic;  -- Operando A
+        b_sub_input    : in std_logic;  -- Operando B
+        sub_borrow_in  : in std_logic;  -- Se '1' utiliza complemento de dois; se '0', subtração simples
+        y_sub_output   : out std_logic; -- Resultado
+        sub_borrow_out : out std_logic  -- Borrow Out
     );
 end entity sub_gate;
 
-architecture data_flow of sub_gate is
-    signal b_inverted   : std_logic;  -- Sinal para armazenar o B invertido
-    signal temp_carry   : std_logic;  -- Sinal intermediário para o carry gerado pelo add_gate
-
-    component inv_gate is
-        port (
-            a_inv_input : in std_logic;
-            y_inv_output : out std_logic
-        );
-    end component;
-
-    component add_gate is
-        port (
-            a_add_input : in std_logic;
-            b_add_input : in std_logic;
-            add_carry_in : in std_logic;  -- Carry In
-            y_add_output : out std_logic; -- Resultado
-            add_carry_out : out std_logic -- Carry Out
-        );
-    end component;
-
+architecture behavioral of sub_gate is
 begin
-    -- Inverter o operando B
-    instancia_inv_gate : inv_gate
-        port map (
-            a_inv_input => b_sub_input,
-            y_inv_output => b_inverted
-        );
-
-    -- Realizar a operação de soma, considerando o complemento de dois (invertido + 1)
-    instancia_add_gate : add_gate
-        port map (
-            a_add_input => a_sub_input,
-            b_add_input => b_inverted,
-            add_carry_in => sub_borrow_in,  -- Borrow In é 1 para efetuar o complemento de dois
-            y_add_output => y_sub_output,
-            add_carry_out => temp_carry    -- Captura o carry gerado
-        );
-
-    -- Inverter o carry para gerar o borrow
-    sub_borrow_out <= not temp_carry;
-end architecture data_flow;
+    process(a_sub_input, b_sub_input, sub_borrow_in)
+        variable var_b_inv  : std_logic;
+        variable var_sum    : std_logic;
+        variable var_carry  : std_logic;
+    begin
+        if sub_borrow_in = '1' then
+            -- Modo complemento de dois: A - B = A + (NOT B) + 1
+            var_b_inv := not b_sub_input;
+            var_sum   := a_sub_input xor var_b_inv xor '1';
+            var_carry := (a_sub_input and var_b_inv) or (a_sub_input and '1') or (var_b_inv and '1');
+            y_sub_output   <= var_sum;
+            sub_borrow_out <= not var_carry;
+        else
+            -- Modo subtração simples: A - B
+            y_sub_output   <= a_sub_input xor b_sub_input;
+            sub_borrow_out <= (not a_sub_input) and b_sub_input;
+        end if;
+    end process;
+    
+end architecture behavioral;
